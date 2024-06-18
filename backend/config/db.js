d0c73403c51c6cfd44ada4ddd44c76eb8bd4740c
skumar <skumar38017@outlook.com>
2024-06-18 11:11:59 +0530

    backend

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,19 +1,8 @@
-<<<<<<< HEAD
 import mongoose from 'mongoose';
 import { MONGODB_URI } from './utils.js';
 export default function connectDB() {
   try {
     mongoose.connect(MONGODB_URI);
-=======
-import dotenv from 'dotenv';
-import mongoose from 'mongoose';
-dotenv.config();
-export default function connectDB() {
-  const url = process.env.MONGODB_URI;
-
-  try {
-    mongoose.connect(url);
->>>>>>> master
   } catch (err) {
     console.error(err.message);
     process.exit(1);
@@ -22,19 +11,11 @@ export default function connectDB() {
   const dbConnection = mongoose.connection;
 
   dbConnection.once('open', () => {
-<<<<<<< HEAD
     console.log(`Database connected: ${MONGODB_URI}`);
   });
 
   dbConnection.on('error', (err) => {
     console.error(`connection error: ${MONGODB_URI}`);
-=======
-    console.log(`Database connected: ${url}`);
-  });
-
-  dbConnection.on('error', (err) => {
-    console.error(`connection error: ${err}`);
->>>>>>> master
   });
   return;
 }
